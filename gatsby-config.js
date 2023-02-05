6ea7155b6179953f0dc935a8b40260558a71512a
leesj-dev <104193215+leesj-dev@users.noreply.github.com>
2023-02-05 14:37:01 +0900

    config 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,13 +66,6 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-copy-linked-files`,
-            options: {
-              destinationDir: `path/to/dir`,
-              ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
-            },
-          },
-          {
             resolve: `gatsby-remark-katex`,
             options: {
               strict: `ignore`
@@ -129,6 +122,7 @@ module.exports = {
             },
           },
           `gatsby-remark-autolink-headers`,
+          `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
         ],
       },
