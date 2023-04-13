00a5ecb6ee2e90d862a869a3a0fd0382253b9dd6
leesj-dev <104193215+leesj-dev@users.noreply.github.com>
2023-04-13 15:02:11 +0900

    Updates

--- sw.js
+++ sw.js
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e2637b05f65b45b00ba5fdd706fb94ce"
+    "revision": "17f84579149186d546aea39433458e3a"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
